eacb5b1d34a573a0b5d85104c71184c6d3423a6a
hiro <hiromhiro617@gmail.com>
2020-11-26 14:01:12 +0900

    error handling

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
 
   resource :retirements, only: %i[new create]
 
-  resources :events do
-    resources :tickets
+  resources :events, only: %i[new create show edit update destroy] do
+    resources :tickets, only: %i[new create destroy]
   end
 
   get 'status' => 'status#index',defaults: { format: 'json' }
+
+  match "*path" => "application#error404", via: :all
 end
